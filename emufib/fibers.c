c85e1de29581409ffa6ed7b864d6072c7285359f
kghite <kghite@gmail.com>
2017-02-12 10:13:50 -0500

    Thread definitions

--- emufib/fibers.c
+++ emufib/fibers.c
@@ -1,1 +1,53 @@
-// Put stuff here
+/*
+* emufib Threading Library
+* fibers.c
+* 
+* Handle thread functions: init, create, yield, destroy
+*/
+
+/* Includes */
+
+/* Struct definition of a stack */
+typedef struct
+{
+	void** stack;
+	void* stack_bottom; /* malloc reference */
+	int active;
+} Thread;
+
+/* Def threading params */
+/* Current thread */
+static int currentFiber = -1;
+/* True if in fiber rather than main process */
+static int inFiber = 0;
+/* Num active threads */
+static int numFibers = 0;
+
+static Thread mainThread;
+
+// Allocate a Stack
+void initStack() {
+
+}
+
+// Init Threads
+void initThread() {
+
+}
+
+// Create Threads
+void createThread() {
+
+}
+
+// Yielding
+void yeild() {
+
+}
+
+// Destroy Threads
+void destryThread() {
+
+}
+
+// Main
